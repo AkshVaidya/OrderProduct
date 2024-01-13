e344632b221879d04c5c8e3bd8a4f60e5dce28f4
Akshay Vaidya <148580678+AkshVaidya@users.noreply.github.com>
2024-01-13 21:46:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require("http");
 const app = require("./app");
-const port = 4000;
+const port = process.env.port||8000;
 const server = http.createServer(app);
 server.listen(port||`0.0.0.0`);
